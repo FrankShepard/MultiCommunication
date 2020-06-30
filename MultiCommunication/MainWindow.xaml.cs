0344c6546509201d07991d868bff9c3c4fc85103
FrankShepard <48500704+FrankShepard@users.noreply.github.com>
2020-07-01 01:19:49 +0800

    完成多机通讯校验的发送功能，接收功能待实际验证

--- MultiCommunication/MainWindow.xaml.cs
+++ MultiCommunication/MainWindow.xaml.cs
@@ -1,17 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO.Ports;
-using System.Linq;
-using System.Text;
+using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Threading;
 
 namespace MultiCommunication
 {
@@ -23,6 +16,11 @@ namespace MultiCommunication
 		public MainWindow()
 		{
 			InitializeComponent();
+			//开启定时器，用于实时刷新进度条、测试环节、测试项、测试值
+			timer = new Timer ( 50 );   //实例化Timer类，设置间隔时间单位毫秒
+			timer.Elapsed += new ElapsedEventHandler ( UpdateWork ); //到达时间的时候执行事件；     
+			timer.AutoReset = true;   //设置是执行一次（false）还是一直执行(true)；     
+			timer.Enabled = true;     //是否执行System.Timers.Timer.Elapsed事件；  
 		}
 		 		
 		string serial_name = string.Empty;//串口名
@@ -36,6 +34,12 @@ namespace MultiCommunication
 		int received_data_count = 0;
 
 		bool use_multi_communication = true;
+		bool send_completed = false;
+
+		//定时器，用于循环显示串口接收到的数据
+		Timer timer;
+
+		SerialPort serialPort;
 
 		private void CobBaudrate_PreviewMouseDown(object sender, MouseButtonEventArgs e)
 		{
@@ -120,17 +124,8 @@ namespace MultiCommunication
 			}else {
 				use_multi_communication = true;
 			}
-
-			//检查需要下发的数据的相关状态
-			for (int index = 1; index <= WrpSend.Children.Count; index += 3) {
-				CheckBox checkBox = WrpSend.Children[ index ] as CheckBox;
-				if((bool)checkBox.IsChecked == false) {
-					address_bits_send[ index / 3 ] = false;
-				}else {
-					address_bits_send[ index / 3 ] = true;
-				}
-			}
-			//向下发送数据
+						
+			//获取需要下发的数据及数量
 			for (int index = 2; index <= WrpSend.Children.Count; index += 3) {
 				TextBox textBox = WrpSend.Children[ index ] as TextBox;
 				if(textBox.Text != string.Empty) {
@@ -158,8 +153,41 @@ namespace MultiCommunication
 				}
 			}
 
-			//实际进行下发
-
+			//检查需要下发的数据的相关状态
+			int address_count = 0;
+			for ( int index = 1 ; index <= WrpSend.Children.Count ; index += 3 ) {
+				CheckBox checkBox = WrpSend.Children [ index ] as CheckBox;
+				if ( ( bool ) checkBox.IsChecked == false ) {
+					address_bits_send [ index / 3 ] = false;
+				} else {
+					address_bits_send [ index / 3 ] = true;
+				}
+				if(++address_count >= send_data_count ) {
+					break;
+				}
+			}
+			//实际进行下发,串口校验模式需要根据地址位与否进行反复设置
+			try {
+				serialPort = new SerialPort ( serial_name, serial_baudrate, Parity.None, 8, StopBits.One );
+				for ( int index = 0 ; index < send_data_count ; index++ ) {
+					if ( use_multi_communication ) {
+						if ( address_bits_send [ index ] ) {
+							serialPort = new SerialPort ( serial_name, serial_baudrate, Parity.Mark, 8, StopBits.One );
+						} else {
+							serialPort = new SerialPort ( serial_name, serial_baudrate, Parity.Space, 8, StopBits.One );
+						}
+					} else {
+						serialPort = new SerialPort ( serial_name, serial_baudrate, Parity.None, 8, StopBits.One );
+					}
+					if ( !serialPort.IsOpen ) {
+						serialPort.Open ( );
+					}
+					serialPort.Write ( datas_send, index, 1 );
+				}
+				send_completed = true; //标记发送完成，需要在接下来的100ms时间内获取回码
+			} catch {
+				MessageBox.Show ( "串口发送过程中出现问题，请重新发送" );
+			}
 		}
 
 		private void TextBox_PreviewKeyDown(object sender, KeyEventArgs e)
@@ -183,5 +211,43 @@ namespace MultiCommunication
 				}
 			}
 		}
+
+		#region -- 定时器操作
+
+		private delegate void dlg_ReceivedDataShow( );
+
+		/// <summary>
+		/// 定时器中执行委托用于显示实时情况
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void UpdateWork( object sender, ElapsedEventArgs e )
+		{
+			if( send_completed ) {
+				int count = 0;
+				do {
+					Thread.Sleep ( 5 );
+				} while ( ( serialPort.BytesToRead == 0 ) && ( ++count < 20 ) );
+
+				int last_count = serialPort.BytesToRead;
+				while ( ( serialPort.BytesToRead != last_count ) && ( ++count < 20 ) ) {
+					Thread.Sleep ( 5 );
+					last_count = serialPort.BytesToRead;
+				}
+
+				//刷新显示
+				serialPort.Read ( datas_received, 0, last_count );
+				for(int index= 0 ;index < last_count ;index++ ) {
+					if ( index < last_count ) {
+						TextBox textBox = WrpReceived.Children [ 2 + 3 * index ] as TextBox;
+						textBox.Text = datas_received [ index ].ToString ( "X2" ); //16进制显示
+					}
+				}
+			}
+		}
+
+		#endregion
+
+
 	}
 }
